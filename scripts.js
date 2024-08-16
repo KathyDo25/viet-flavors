6d5edaf29c2e12f88bb0d9220bf222a4ebb97e1c
KathyDo25 <147874108+KathyDo25@users.noreply.github.com>
2024-08-16 14:40:20 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const ctaButton = document.querySelector('.cta-button');
+    ctaButton.addEventListener('click', function(event) {
+        alert('Welcome to Viet Flavors! Explore our delicious menu.');
+    });
+});
+
